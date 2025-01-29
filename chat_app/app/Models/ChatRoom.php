31ae54322695e1acf27d2529b531bb03563c8a12
aleksandra285 <aleksandramilenkovic222855@gmail.com>
2025-01-29 14:35:31 +0100

    Dodati modeli

---
+++ chat_app/app/Models/ChatRoom.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ChatRoom extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'created_by'];
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+}
