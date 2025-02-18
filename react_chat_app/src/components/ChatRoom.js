80fa364f5d08cd053a51b530cc79a84cf4ef8650
aleksandra285 <aleksandramilenkovic222855@gmail.com>
2025-02-18 23:10:23 +0100

    Dodati drugi useri u chat sobu

--- react_chat_app/src/components/ChatRoom.js
+++ react_chat_app/src/components/ChatRoom.js
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import "../styles/ChatRoom.css";
+import backgroundImage from '../pozadina.jpg';
 
 
 const ChatRoom = () => {
   const { roomId } = useParams(); // Uzimanje ID sobe iz URL-a
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const [currentUser, setCurrentUser] = useState('user1'); // Postavljen "trenutno ulogivani korisnik-zameniii"
+  const [currentUser, setCurrentUser] = useState(null); 
   const [roomName, setRoomName] = useState('');
+  const [users, setUsers] = useState([]); 
+  const [newUserName, setNewUserName] = useState(""); 
+  const [activeUser, setActiveUser] = useState(null);
+  const [showDeleteButton, setShowDeleteButton] = useState(null);
 
   // Učitaj prethodne poruke iz localStorage
   useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("loggedInUser"));
+    if (storedUser && storedUser.name) {
+      setCurrentUser(storedUser.name);
+      setActiveUser(storedUser.name);
+    
+    }    
     const savedMessages = JSON.parse(localStorage.getItem(roomId)) || [];
     setMessages(savedMessages);
     // Učitavanje imena sobe 
@@ -23,14 +34,29 @@ const ChatRoom = () => {
     }else {
         setRoomName('Room not found'); 
       }
+
+    const savedUsers = JSON.parse(localStorage.getItem(`users_${roomId}`)) || [];
+    setUsers(savedUsers);
+
+    if (savedUsers.length > 0 && !storedUser) {//ako nema ulogovanog korisnika, postavlja prvog iz liste kao ulogovanog 
+      setCurrentUser(savedUsers[0]);
+      setActiveUser(savedUsers[0]);
+    }
   }, [roomId]);
 
+  const handleMouseEnter = (index) => {
+    setShowDeleteButton(index);
+  };
+  
+  const handleMouseLeave = () => {
+    setShowDeleteButton(null);
+  };
   
   const sendMessage = () => {
-    if (message.trim()) {
+    if (message.trim() && currentUser) {
       // Dodajte novu poruku u niz
       const newMessage = {
-        user: currentUser,
+        user: activeUser,
         content: message,
         timestamp: new Date().toISOString(),
       };
@@ -44,28 +70,79 @@ const ChatRoom = () => {
     }
   };
 
-  // prebacivanje između korisnika
-  const toggleUser = () => {
-    setCurrentUser(currentUser === 'user1' ? 'user2' : 'user1');
+  const addUser = () => {
+    if (newUserName.trim() && !users.includes(newUserName)) {
+      const updatedUsers = [...users, newUserName];
+      setUsers(updatedUsers);
+      localStorage.setItem(`users_${roomId}`, JSON.stringify(updatedUsers));
+      setNewUserName("");
+    }
+  };
+  const handleDelete = (index) => {
+    const updatedMessages = [...messages];
+    updatedMessages.splice(index, 1);  // Uklanja poruku na datom indeksu
+    setMessages(updatedMessages);
+    
+    // Čuvanje ažuriranih poruka u localStorage
+    localStorage.setItem(roomId, JSON.stringify(updatedMessages));
   };
 
+  const switchUser = (user) => {
+    setActiveUser(user);
+  };
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  };
   return (
     <div className="chat-room-container">
       <h2>Chat Grupa: {roomName || 'Loading...'}</h2>
 
-      <button className="switch-user-button" onClick={toggleUser}>
-        Prebaci na {currentUser === 'user1' ? 'User2' : 'User1'}
-      </button>
+      <div className="add-user-container">
+        <input
+          className='input-message'
+          type="text"
+          value={newUserName}
+          onChange={(e) => setNewUserName(e.target.value)}
+          placeholder="Unesite ime korisnika"
+        />
+        <button className='add-user-button' onClick={addUser}>Dodaj korisnika</button>
+      </div>
 
-      <div className="chat-window">
+      <div className="user-switch-container">
+        <span>Trenutno piše kao: <strong>{activeUser}</strong></span>
+        <button className="current-user-button" onClick={() => switchUser(currentUser)}>
+          {currentUser}
+        </button>
+        {users.map((user, index) => (
+          <button key={index} onClick={() => switchUser(user)}>
+            Prebaci na {user}
+          </button>
+        ))}
+      </div>
+
+      <div className="chat-window" style={backgroundStyle}>
+      
         {messages.map((msg, index) => (
           <div
-            key={index}
-            className={msg.user === 'user1' ? 'user1-message' : 'user2-message'}
+            key={index} className={`message ${msg.user === currentUser ? "my-message" : "other-message"}`}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
           >
-            <strong>{msg.user === 'user1' ? 'User1' : 'User2'}:</strong> {msg.content}
-          </div>
-        ))}
+            <strong>{msg.user}:</strong> {msg.content}
+      
+            {showDeleteButton === index && (
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(index)}
+              >
+                Obrisi poruku ❌
+              </button>
+            )}
+            </div>
+      ))}
       </div>
 
       <input
--- react_chat_app/src/components/UserCard.js
+++ react_chat_app/src/components/UserCard.js
@@ -1,31 +1,28 @@
 import React from "react";
 import "../styles/UserCard.css";
 
-
 const UserCard = () => {
-  const user = JSON.parse(localStorage.getItem("loggedInUser"));
+    const user = JSON.parse(localStorage.getItem("loggedInUser"));
 
-  if (!user) {
-    return <p>Nema prijavljenog korisnika.</p>;
-  }
+    if (!user) {
+        return <p>Nema prijavljenog korisnika.</p>;
+    }
 
-  return (
-    
-    <div className="user-card-container">
-    <div className="images-container">
-    <img src="images/avatar-profile-icon1.jpg" alt="Left" className="left-image" />
-    </div>
-      <div className="user-card">
-        <h3>Korisnički profil</h3>
-        <p><strong>Ime:</strong> {user.name}</p>
-        <p><strong>Email:</strong> {user.email}</p>
-      </div>
-      <div className="images-container">
-        <img src="images/avatar-profile-icon2.jpg" alt="Right" className="right-image" />
-      </div>
-  </div>
- 
-  );
+    return (
+        <div className="user-card-container">
+            <div className="images-container">
+                <img src="images/avatar-profile-icon1.jpg" alt="Left" className="left-image" />
+            </div>
+            <div className="user-card">
+                <h3>Korisnički profil</h3>
+                <p><strong>Ime:</strong> {user.name}</p>
+                <p><strong>Email:</strong> {user.email}</p>
+            </div>
+            <div className="images-container">
+                <img src="images/avatar-profile-icon2.jpg" alt="Right" className="right-image" />
+            </div>
+        </div>
+    );
 };
 
 export default UserCard;
